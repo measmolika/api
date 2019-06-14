575a9fdeb067d2d63ebec8dc78859fff5fb2a8db
Meas Molika <measmolika9@gmail.com>
2019-06-14 14:28:33 +0300

    search

--- queries.js
+++ queries.js
@@ -110,7 +110,6 @@ const lookupBooking = (request, response) => {
 
   client_promise.then(data => {
 
-    if(data.length > 0) {
     var params = [];
     for(var n = 1; n<=data.length; n++) {
         params.push('$'+n);
@@ -130,8 +129,7 @@ const lookupBooking = (request, response) => {
       }
       response.render("search_results",{ search_results : search_results, count : search_results.length, keyword : keyword });
 
-    });}
-    response.render("search_results",{ search_results : search_results, count : search_results.length, keyword : keyword });
+    });
 
   });
 
